9e896b5f3c28fa69576aa67ceac0fc17add4fd9f
Mauricio Togneri <mauricio.togneri@gmail.com>
2022-12-09 23:31:18 +0100

    Improvements

--- flutter/scripts/deploy.sh
+++ flutter/scripts/deploy.sh
@@ -8,6 +8,7 @@ flutter build web --web-renderer html
 rm -r ../public
 mkdir ../public
 cp -r build/web/** ../public
+cp -r ../public/assets/assets/images ../public/assets
 
 cd ..
 firebase deploy --only hosting
