6b119ccfc4eb9fb6e641db028b35b86756735125
Mauricio Togneri <mauricio.togneri@gmail.com>
2022-12-11 21:33:20 +0100

    Improvements

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -13,7 +13,6 @@ class AtomicInstinctWebsite extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        fontFamily: 'ZenDots',
       ),
       home: Scaffold(
         backgroundColor: Colors.black,
@@ -51,6 +50,7 @@ class Header extends StatelessWidget {
                 'Atomic\nInstinct',
                 style: TextStyle(
                   color: Colors.white,
+                  fontFamily: 'ZenDots',
                   fontSize: 200,
                 ),
               ),
@@ -71,26 +71,188 @@ class Games extends StatelessWidget {
       flex: 5,
       child: Container(
         color: Colors.green,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GameEntry(
+              name: 'Hextrategic',
+              description:
+                  'Hextrategic is a turn-based strategy game where you move units across a board to expand your territory and defeat your enemies. Choose a map. Command your units. Defeat your enemies. Conquer the board.',
+              imagePath: 'images/hextrategic.png',
+              androidUrl:
+                  'https://play.google.com/store/apps/details?id=com.atomicinstinct.hextrategic&referrer=website&url=https://atomicinstinct.com',
+              iosUrl: 'https://apps.apple.com/app/hextrategic/id6444746115',
+              onTap: _onHextrategicWebsite,
+            ),
+            GameEntry(
+              name: 'Tension Tunnel',
+              description:
+                  'Tension Tunnel is a casual, minimalist and challenging game that will put your focus, reflexes, and nerves to the test. The game has simple mechanics but it requires high dexterity and precise execution from the player.',
+              imagePath: 'images/tension-tunnel.png',
+              androidUrl:
+                  'https://play.google.com/store/apps/details?id=com.atomicinstinct.tensiontunnel&referrer=website&url=https://atomicinstinct.com',
+              iosUrl: 'https://apps.apple.com/app/tension-tunnel/id1608041401',
+              onTap: _onTensionTunnelWebsite,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onHextrategicWebsite() => launchUrl(Uri.parse('https://hextrategic.com'));
+
+  void _onTensionTunnelWebsite() => launchUrl(Uri.parse('https://tensiontunnel.com'));
+}
+
+class GameEntry extends StatelessWidget {
+  final String name;
+  final String description;
+  final String imagePath;
+  final String androidUrl;
+  final String iosUrl;
+  final VoidCallback onTap;
+
+  const GameEntry({
+    required this.name,
+    required this.description,
+    required this.imagePath,
+    required this.androidUrl,
+    required this.iosUrl,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 100,
+          right: 100,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            DescriptionEntry(
+              name: name,
+              description: description,
+              imagePath: imagePath,
+              onTap: onTap,
+            ),
+            const SizedBox(height: 30),
+            StoreButtons(
+              android: androidUrl,
+              ios: iosUrl,
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
-class GameEntry extends StatelessWidget {
-  const GameEntry();
+class DescriptionEntry extends StatelessWidget {
+  final String name;
+  final String description;
+  final String imagePath;
+  final VoidCallback onTap;
+
+  const DescriptionEntry({
+    required this.name,
+    required this.description,
+    required this.imagePath,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return GestureDetector(
+      onTap: onTap,
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              name,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+              ),
+            ),
+            const SizedBox(height: 30),
+            SizedBox(
+              height: 300,
+              child: Image.asset(imagePath),
+            ),
+            const SizedBox(height: 30),
+            Text(
+              description,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class StoreButtons extends StatelessWidget {
+  final String android;
+  final String ios;
+
+  const StoreButtons({
+    required this.android,
+    required this.ios,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        SizedBox(
-          width: 200,
-          height: 200,
-          child: Image.asset('images/logo.png'),
+        StoreButton(
+          imagePath: 'images/google_play_badge.png',
+          url: android,
+        ),
+        const SizedBox(width: 50),
+        StoreButton(
+          imagePath: 'images/app_store_badge.png',
+          url: ios,
         ),
       ],
     );
   }
+}
+
+class StoreButton extends StatelessWidget {
+  final String imagePath;
+  final String url;
+
+  const StoreButton({
+    required this.imagePath,
+    required this.url,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _onTap,
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: SizedBox(
+          height: 75,
+          child: Image.asset(imagePath),
+        ),
+      ),
+    );
+  }
+
+  void _onTap() => launchUrl(Uri.parse(url));
 }
 
 class Footer extends StatelessWidget {
