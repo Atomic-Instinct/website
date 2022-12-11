cc7c21d3b537c5c1afc2f066edaf185b7fd525d0
Mauricio Togneri <mauricio.togneri@gmail.com>
2022-12-11 21:14:29 +0100

    Improvements

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(const AtomicInstinctWebsite());
 
@@ -35,25 +36,27 @@ class Header extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       flex: 2,
-      child: FittedBox(
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: 500,
-              height: 500,
-              child: Image.asset('images/logo.png'),
-            ),
-            const Text(
-              'Atomic\nInstinct',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 200,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 500,
+            child: Image.asset('images/logo.png'),
+          ),
+          const FittedBox(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 50),
+              child: Text(
+                'Atomic\nInstinct',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 200,
+                ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -90,11 +93,6 @@ class GameEntry extends StatelessWidget {
   }
 }
 
-/*
-https://www.youtube.com/@atomicinstinct
-https://www.instagram.com/atomicinstinct/
-https://twitter.com/instinct_atomic
-*/
 class Footer extends StatelessWidget {
   const Footer();
 
@@ -102,8 +100,75 @@ class Footer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       flex: 1,
-      child: Container(
-        color: Colors.blue,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SocialMediaEntry(
+            path: 'images/instagram.png',
+            onTap: _onInstagram,
+          ),
+          SocialMediaEntry(
+            path: 'images/twitter.png',
+            onTap: _onTwitter,
+          ),
+          SocialMediaEntry(
+            path: 'images/youtube.png',
+            onTap: _onYoutube,
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onInstagram() => launchUrl(Uri.parse('https://www.instagram.com/atomicinstinct'));
+
+  void _onYoutube() => launchUrl(Uri.parse('https://www.youtube.com/@atomicinstinct'));
+
+  void _onTwitter() => launchUrl(Uri.parse('https://twitter.com/instinct_atomic'));
+}
+
+class SocialMediaEntry extends StatefulWidget {
+  final String path;
+  final VoidCallback onTap;
+
+  const SocialMediaEntry({
+    required this.path,
+    required this.onTap,
+  });
+
+  @override
+  State<SocialMediaEntry> createState() => _SocialMediaEntryState();
+}
+
+class _SocialMediaEntryState extends State<SocialMediaEntry> {
+  double opacity = 0.5;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: GestureDetector(
+        onTap: widget.onTap,
+        child: MouseRegion(
+          cursor: SystemMouseCursors.click,
+          onHover: (event) {
+            setState(() {
+              opacity = 1;
+            });
+          },
+          onExit: (event) {
+            setState(() {
+              opacity = 0.5;
+            });
+          },
+          child: SizedBox(
+            height: 500,
+            child: Opacity(
+              opacity: opacity,
+              child: Image.asset(widget.path),
+            ),
+          ),
+        ),
       ),
     );
   }
